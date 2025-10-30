9bbc503fa4493e85afc0290c52bc2b92a73d0e61
Ramos Mendoza <aramosme@intercorp.com.pe>
2025-10-29 21:28:50 -0500

    api de catalogo

--- src/lambda/catalogo/list.ts
+++ src/lambda/catalogo/list.ts
@@ -1,43 +1,45 @@
 // src/handler.ts
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { QueryCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import ddb from "../../datos/db";
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   try {
-    const id = event.queryStringParameters?.id;
-    if (!id) {
+    const descripcion = event.queryStringParameters?.descripcion;
+    if (!descripcion) {
       return {
         statusCode: 400,
         headers: {
           "Access-Control-Allow-Origin": "*", // acceso total
           "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
         },
-        body: JSON.stringify({ message: "Falta el parámetro 'id'" })
+        body: JSON.stringify({ message: "Falta el parámetro 'id'" }),
       };
     }
 
-    
-    const params = {
-      TableName: "softmar_mochilyta_configuracion",
-      KeyConditionExpression: "id_configuracion = :id",
+    const paramsScan = {
+      TableName: "softmar_mochilyta_catalogo_lugares",
+      FilterExpression: "contains(descripcion, :desc)",
       ExpressionAttributeValues: {
-        ":id": id
-      }
+        ":desc": descripcion
+          .normalize("NFD")
+          .replace(/(?!\u0303)[\u0300-\u036f]/g, "")
+          .toLowerCase(),
+      },
     };
+    const scanResult = await ddb.send(new ScanCommand(paramsScan));
 
-    const result = await ddb.send(new QueryCommand(params));
     return {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin": "*", // acceso total
         "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
       },
-      body: JSON.stringify(result.Items)
+      body: JSON.stringify(scanResult.Items),
     };
   } catch (err) {
     return {
@@ -45,11 +47,11 @@ export const handler = async (
       headers: {
         "Access-Control-Allow-Origin": "*", // acceso total
         "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
       },
       body: JSON.stringify({
-        error: err instanceof Error ? err.message : "Error desconocido"
-      })
+        error: err instanceof Error ? err.message : "Error desconocido",
+      }),
     };
   }
 };
